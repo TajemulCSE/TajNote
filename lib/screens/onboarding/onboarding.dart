537265f89408651df0ffec9434bc06618cf21dc4
TajemulCSE <tajemulislamgames@gmail.com>
2025-07-18 00:08:22 +0600

    Added 3 image slide with dot progress

--- lib/screens/onboarding/onboarding.dart
+++ lib/screens/onboarding/onboarding.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class Onboarding extends StatefulWidget {
@@ -6,6 +8,7 @@ class Onboarding extends StatefulWidget {
 
 class _Onboarding extends State<Onboarding> {
   int currentIndex = 0;
+  final PageController pageController = PageController();
 
   final List<String> imagePaths = [
     "assets/images/CaptureYourThoughtsQuickly.png",
@@ -13,10 +16,86 @@ class _Onboarding extends State<Onboarding> {
     "assets/images/TodoList.png",
   ];
 
+  void initState() {
+    super.initState();
+
+    Timer.periodic(Duration(seconds: 3), (Timer timer) {
+      if (pageController.hasClients) {
+        int nextPage = (pageController.page?.round() ?? 0) + 1;
+
+        if (imagePaths.length == nextPage) {
+          nextPage = 0;
+        }
+
+        pageController.animateToPage(
+          nextPage,
+          duration: Duration(microseconds: 700),
+          curve: Curves.easeInOut,
+        );
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(backgroundColor: const Color.fromARGB(255, 100, 100, 99)),
+      drawer: Drawer(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFFFFB347), Color.fromARGB(255, 131, 113, 113)],
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+            ),
+          ),
+          child: ListView(
+            children: [
+              DrawerHeader(
+                child: Column(
+                  children: [
+                    Text(
+                      "TajNote",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontFamily: "FugazOneRegular",
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text("V1.1"),
+                  ],
+                ),
+              ),
+              ListTile(
+                title: Text(
+                  "Forgot Password ?",
+                  style: TextStyle(color: Colors.black, fontSize: 18),
+                ),
+                onTap: () {
+                  Navigator.pushNamed(context, "/login");
+                },
+              ),
+              ListTile(
+                title: Text(
+                  "Privacy Policy ?",
+                  style: TextStyle(color: Colors.black, fontSize: 18),
+                ),
+                onTap: () {
+                  Navigator.pushNamed(context, "/privacy_policy");
+                },
+              ),
+              ListTile(
+                title: Text(
+                  "Terms of Use",
+                  style: TextStyle(color: Colors.black, fontSize: 18),
+                ),
+                onTap: () {
+                  Navigator.pushNamed(context, "/terms_of_use");
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
 
       body: Builder(
         builder:
@@ -65,8 +144,10 @@ class _Onboarding extends State<Onboarding> {
                   ),
                 ),
 
+                // 3 image slide show pageview
                 Expanded(
                   child: PageView.builder(
+                    controller: pageController,
                     itemCount: imagePaths.length,
                     onPageChanged: (index) {
                       setState(() {
@@ -87,16 +168,18 @@ class _Onboarding extends State<Onboarding> {
                         ),
                   ),
                 ),
+
+                // Progess Bar Dot
                 Padding(
                   padding: const EdgeInsets.only(bottom: 50),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: List.generate(imagePaths.length, (index) {
                       return AnimatedContainer(
-                        duration: Duration(microseconds: 3000),
+                        duration: Duration(milliseconds: 300),
                         margin: EdgeInsets.symmetric(horizontal: 4),
                         height: 8,
-                        width: currentIndex == index ? 24 : 12,
+                        width: currentIndex == index ? 40 : 20,
 
                         decoration: BoxDecoration(
                           color:
@@ -113,28 +196,32 @@ class _Onboarding extends State<Onboarding> {
                 Padding(
                   padding: const EdgeInsets.all(15.0),
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.pushNamed(context, "/register");
+                    },
                     child: Text(
                       "CREATE ACCOUNT",
                       style: TextStyle(color: Colors.white),
                     ),
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Color(0xFFFFFB347),
-                      fixedSize: Size(300, 50)
+                      fixedSize: Size(300, 50),
                     ),
                   ),
                 ),
                 Padding(
                   padding: const EdgeInsets.all(15.0),
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.pushNamed(context, "/login");
+                    },
                     child: Text(
                       "Login",
                       style: TextStyle(color: Color(0xFFFFFB347)),
                     ),
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.white,
-                      fixedSize: Size(300, 50)
+                      fixedSize: Size(300, 50),
                     ),
                   ),
                 ),
